9fed873e464e72329f0af3a3792636d4bbbf0688
Greg Beech <greg@blinkbox.com>
2014-05-22 18:04:57 +0100

    Improved configuration code

--- src/main/scala/com/blinkbox/books/config/Configuration.scala
+++ src/main/scala/com/blinkbox/books/config/Configuration.scala
@@ -2,12 +2,14 @@ package com.blinkbox.books.config
 
 import com.typesafe.config.{Config, ConfigException, ConfigFactory, ConfigParseOptions}
 import java.io.File
-import java.net.{URISyntaxException, MalformedURLException, URI}
+import java.net.{URI, URISyntaxException}
 
 /**
  * A trait that can be mixed in to load configuration in the standard way.
  */
 trait Configuration {
+  private val configVar = "CONFIG_URL"
+  private val parseOptions = ConfigParseOptions.defaults().setAllowMissing(false)
 
   /**
    * The loaded configuration.
@@ -22,18 +24,19 @@ trait Configuration {
     }
   }
 
-  private def loadExternalConfig: Option[Config] = {
-    val configVar = "CONFIG_URL"
-    Option(System.getenv(configVar)) flatMap { url =>
-      val uri = try new URI(url) catch {
-        case e @ (_: MalformedURLException | _: URISyntaxException) => throw new ConfigException.BugOrBroken(s"$configVar has invalid value '$url'.", e)
-      }
-      val parseOptions = ConfigParseOptions.defaults().setAllowMissing(false)
+  private def loadExternalConfig: Option[Config] =
+    Option(System.getenv(configVar)) map { s =>
+      val uri = parseUri(s)
       uri.getScheme match {
-        case _ @ ("http" | "https") => Some(ConfigFactory.parseURL(uri.toURL, parseOptions))
-        case "file" => Some(ConfigFactory.parseFile(new File(uri), parseOptions))
-        case _ => throw new ConfigException.BugOrBroken(s"$configVar has unsupported scheme '${uri.getScheme}'.")
+        case _ @ ("http" | "https") => ConfigFactory.parseURL(uri.toURL, parseOptions)
+        case "file" => ConfigFactory.parseFile(new File(uri), parseOptions)
+        case other => throw new ConfigException.Generic(s"$configVar has unsupported scheme '$other'.")
       }
     }
-  }
+
+  private def parseUri(uri: String): URI =
+    try new URI(uri)
+    catch {
+      case e: URISyntaxException => throw new ConfigException.Generic(s"$configVar has invalid value '$uri'.", e)
+    }
 }
