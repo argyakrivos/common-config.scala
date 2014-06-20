e1087adc5318aa682d776c4c7283134aa5e24989
Greg Beech <greg@blinkbox.com>
2014-06-20 12:16:09 +0100

    Added some tests for layout
    And fixed the rendering of the timestamp to be a plain decimal rather
    than using exponential notation which seems to work but isn’t
    documented as being supported

--- src/main/scala/com/blinkbox/books/logging/gelf/GelfLayout.scala
+++ src/main/scala/com/blinkbox/books/logging/gelf/GelfLayout.scala
@@ -6,7 +6,7 @@ import ch.qos.logback.classic.util.LevelToSyslogSeverity
 import ch.qos.logback.core.{CoreConstants, LayoutBase}
 import java.io.StringWriter
 import java.net.InetAddress
-import org.json4s.JsonAST.JObject
+import org.json4s.JsonAST.{JDecimal, JObject}
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 import scala.beans.BeanProperty
@@ -39,11 +39,11 @@ class GelfLayout extends LayoutBase[ILoggingEvent] {
   override def doLayout(event: ILoggingEvent): String = {
     var json =
       ("version" -> "1.1") ~
-        ("host" -> InetAddress.getLocalHost.getHostName) ~
-        ("short_message" -> shortMessageLayout.doLayout(event)) ~
-        ("full_message" -> fullMessageLayout.doLayout(event)) ~
-        ("timestamp" -> event.getTimeStamp / 1000.0) ~
-        ("level" -> LevelToSyslogSeverity.convert(event))
+      ("host" -> InetAddress.getLocalHost.getHostName) ~
+      ("short_message" -> shortMessageLayout.doLayout(event)) ~
+      ("full_message" -> fullMessageLayout.doLayout(event)) ~
+      ("timestamp" -> JDecimal(BigDecimal(event.getTimeStamp) / 1000)) ~
+      ("level" -> LevelToSyslogSeverity.convert(event))
 
     if (facility != null) json ~= ("_facility", facility)
     if (includeLoggerName) json ~= ("_loggerName", event.getLoggerName)
@@ -67,16 +67,16 @@ class GelfLayout extends LayoutBase[ILoggingEvent] {
     layout
   }
 
-  private def callerData(event: ILoggingEvent): Option[JObject] =
+  private def callerData(event: ILoggingEvent) =
     event.getCallerData match {
       case Array(c, _*) => Some(("_file" -> c.getFileName) ~ ("_line" -> c.getLineNumber) ~ ("_method" -> c.getMethodName))
       case _ => None
     }
 
-  private def exceptionData(event: ILoggingEvent): Option[JObject] =
-    Option(event.getThrowableProxy) map { proxy =>
+  private def exceptionData(event: ILoggingEvent) =
+    for (proxy <- Option(event.getThrowableProxy)) yield {
       ("_exceptionClass" -> proxy.getClassName) ~
-        ("_exceptionMessage" -> proxy.getMessage) ~
-        ("_exceptionDetail" -> exceptionLayout.doLayout(event))
+      ("_exceptionMessage" -> proxy.getMessage) ~
+      ("_exceptionDetail" -> exceptionLayout.doLayout(event))
     }
 }
