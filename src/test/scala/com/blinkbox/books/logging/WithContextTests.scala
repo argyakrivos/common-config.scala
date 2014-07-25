16c352efeb90e8f1515bce85aba9b7978ebfb3f7
Greg Beech <greg@blinkbox.com>
2014-07-25 14:40:59 +0100

    Added test for keys with null values

--- src/test/scala/com/blinkbox/books/logging/WithContextTests.scala
+++ src/test/scala/com/blinkbox/books/logging/WithContextTests.scala
@@ -17,17 +17,17 @@ import org.slf4j.{Logger => Slf4jLogger, LoggerFactory => Slf4jLoggerFactory}
 class WithContextTests extends FunSuite with MockitoSugar with Logging {
 
   test("logger.withContext sets the context only within the passed block") {
-    val mdc1 = new AtomicReference[Option[java.util.Map[String, String]]](None)
-    val mdc2 = new AtomicReference[Option[java.util.Map[String, String]]](None)
+    val mdcRef1 = new AtomicReference[Option[java.util.Map[String, String]]](None)
+    val mdcRef2 = new AtomicReference[Option[java.util.Map[String, String]]](None)
 
     val appender = mock[Appender[ILoggingEvent]]
     when(appender.getName).thenReturn("MOCK")
     when(appender.doAppend(any[ILoggingEvent])).thenAnswer(new Answer[Unit] {
       override def answer(invocation: InvocationOnMock): Unit =
-        mdc1.set(Some(invocation.getArguments()(0).asInstanceOf[ILoggingEvent].getMDCPropertyMap))
+        mdcRef1.set(Some(invocation.getArguments()(0).asInstanceOf[ILoggingEvent].getMDCPropertyMap))
     }).thenAnswer(new Answer[Unit] {
       override def answer(invocation: InvocationOnMock): Unit =
-        mdc2.set(Some(invocation.getArguments()(0).asInstanceOf[ILoggingEvent].getMDCPropertyMap))
+        mdcRef2.set(Some(invocation.getArguments()(0).asInstanceOf[ILoggingEvent].getMDCPropertyMap))
     })
 
     val rootLogger = Slf4jLoggerFactory.getLogger(Slf4jLogger.ROOT_LOGGER_NAME).asInstanceOf[ClassicLogger]
@@ -37,8 +37,28 @@ class WithContextTests extends FunSuite with MockitoSugar with Logging {
     logger.withContext("k1" -> "v1")(_.debug("hello world"))
     logger.debug("hello world")
 
-    assert(mdc1.get.get.get("k1") == "v1")
-    assert(!mdc2.get.get.containsKey("k1"))
+    assert(mdcRef1.get.get.get("k1") == "v1")
+    assert(!mdcRef2.get.get.containsKey("k1"))
+  }
+
+  test("keys with null values are not added to the MDC") {
+    val mdcRef = new AtomicReference[Option[java.util.Map[String, String]]](None)
+
+    val appender = mock[Appender[ILoggingEvent]]
+    when(appender.getName).thenReturn("MOCK")
+    when(appender.doAppend(any[ILoggingEvent])).thenAnswer(new Answer[Unit] {
+      override def answer(invocation: InvocationOnMock): Unit =
+        mdcRef.set(Some(invocation.getArguments()(0).asInstanceOf[ILoggingEvent].getMDCPropertyMap))
+    })
+
+    val rootLogger = Slf4jLoggerFactory.getLogger(Slf4jLogger.ROOT_LOGGER_NAME).asInstanceOf[ClassicLogger]
+    rootLogger.detachAndStopAllAppenders()
+    rootLogger.addAppender(appender)
+
+    logger.withContext("k1" -> 123, "k2" -> null)(_.debug("hello world"))
+
+    assert(mdcRef.get.get.get("k1") == "123")
+    assert(!mdcRef.get.get.containsKey("k2"))
   }
 
 }
