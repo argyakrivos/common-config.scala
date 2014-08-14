1f58be7699444284bed27288c2c0a158f6fab2de
Anargyros Akrivos <anargyros@blinkbox.com>
2014-08-14 18:42:10 +0100

    Added some extra fields for DatabaseConfig

--- src/main/scala/com/blinkbox/books/config/ConfigSections.scala
+++ src/main/scala/com/blinkbox/books/config/ConfigSections.scala
@@ -1,16 +1,34 @@
 package com.blinkbox.books.config
 
-import com.typesafe.config.Config
 import java.io.File
 import java.net.{URI, URL}
 import java.util.concurrent.TimeUnit
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigException.BadValue
+
 import scala.concurrent.duration._
 
 case class ApiConfig(externalUrl: URL, localUrl: URL, timeout: FiniteDuration)
 case class AuthClientConfig(url: URL, keysDir: File) {
   val sessionUrl = new URL(url, "session")
 }
-case class DatabaseConfig(uri: URI)
+case class DatabaseConfig(uri: URI) {
+  val host = if (uri.getHost != null) uri.getHost else throw new BadValue("db.url", "Host is missing or it's incorrect.")
+  val port = if (uri.getPort != -1) Some(uri.getPort) else None
+  val Array(user, pass) = if (uri.getUserInfo != null) {
+    uri.getUserInfo.split(":") match {
+      case x @ Array(u, p) => x
+      case _ => throw new BadValue("db.url", s"Username and password information is not well-formed: '${uri.getUserInfo}'.")
+    }
+  } else throw new BadValue("db.url", "Username and password information is missing.")
+  val db = if (uri.getPath != null && uri.getPath != "") {
+    if (uri.getPath.count(_ == '/') == 1) uri.getPath.stripPrefix("/")
+    else throw new BadValue("db.url", s"Database name is not well-formed: '${uri.getPath}'.")
+  } else throw new BadValue("db.url", "Database name is missing.")
+  val dbProperties = if (uri.getQuery != null) Some(uri.getQuery) else None
+  val jdbcUrl = s"jdbc:${uri.getScheme}://$host${port.map(":" + _).getOrElse("")}/$db${dbProperties.map("?" + _).getOrElse("")}"
+}
 case class SwaggerConfig(baseUrl: URL, docsPath: String)
 
 object ApiConfig {
